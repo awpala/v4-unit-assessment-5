08e60aae08aa2d94dad79ac22915e87d8d36dbdd
awpala <adam.w.pala@gmail.com>
2021-02-20 20:00:00 -0600

    ROUTING initialize routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Nav from './Components/Nav/Nav';
+import routes from './routes';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'></div>
-  )
-};
+const App = () => (
+  <div className='App'>
+    <Nav />
+    {routes}
+  </div>
+);
 
 export default App;
